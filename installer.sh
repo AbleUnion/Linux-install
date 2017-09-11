f1424041d9308b620bf60d70e705507f65051039
Ip <jun040329@gmail.com>
2017-09-11 22:26:28 +0900

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget -q -O - https://raw.githubusercontent.com/AbleUnion/php-binarys/blob/master/PHP_Linux-x86_64.tar.gz
+tar zxvf PHP_Linux-x86_64.tar.gz
+
+echo -e [AbleUnion] 구동기 설치가 완벽하게 안됬으면 apt install unzip을 한뒤 다시 해주세요.
